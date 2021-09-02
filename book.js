45a5367cb66bbc09089862192af7ebbbf92d685b
Tosirr <tosir802637@gmail.com>
2021-09-02 10:17:59 +0600

    search result validation

--- book.js
+++ book.js
@@ -22,7 +22,7 @@ const fetchData = async (searchText) => {
     `https://openlibrary.org/search.json?q=${searchText}`
   );
   const data = await res.json();
-  displayData(data.docs.slice(0, 30));
+  displayData(data);
 };
 
 // display data
@@ -30,21 +30,21 @@ const displayData = (getData) => {
   const contain = document.getElementById('contain');
   document.getElementById('spinner').classList.add('d-none');
   const foundBook = document.getElementById('book-found');
-  if (getData.length > 0) {
-    foundBook.innerText = `${getData.length} Book found`;
+  if (getData.docs.length > 0) {
+    foundBook.innerText = `${getData.num_found} Book found`;
   } else {
     foundBook.innerText = `Opps! This book are not available`;
   }
-  getData.forEach((data) => {
+  getData.docs.slice(0, 30).forEach((data) => {
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
          <div class="card p-2 text-center border-warning">
-         <img src=" ${data.cover_i? `https://covers.openlibrary.org/b/id/${data.cover_i}-L.jpg`: `book img.jpg`}"> 
+         <img src=" ${data.cover_i? `https://covers.openlibrary.org/b/id/${data.cover_i}-L.jpg` : `book img.jpg`}"> 
          <h5 class="p-1"><span class="text-primary">Book:</span> ${data.title}</h5>
          <h6 class="p-1"><span class="text-primary">Writer:</span>${data.author_name ? data.author_name[0] : 'Unknown!'}</h6>
          <p class="p-1"><span class="text-primary">Publish-date:</span> ${ data.first_publish_year ? data.first_publish_year : 'Not given'}</p>
-         <p class="p-1"><span class="text-primary">Publisher:</span> ${ data.publisher ? data.publisher[0] : 'Not given'}</p>
+         <p class="p-1"><span class="text-primary">Publisher:</span> ${data.publisher ? data.publisher[0] : 'Not given'}</p>
          </div>
         `;
     contain.appendChild(div);
